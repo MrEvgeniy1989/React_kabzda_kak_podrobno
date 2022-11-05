4ab71ea26a19deeab33d617dd8f453240d1d5ab8
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2022-11-05 20:18:08 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
-function Hello() {
+function hello() {
     debugger
     alert('Hello IT-KAMASUTRA')
 }
+
 // hello();
 
 function App() {
@@ -22,38 +25,6 @@ function App() {
 function AppTitle() {
     console.log("AppTitle rendering")
     return <>This is APP Component</>
-}
-
-function Rating() {
-    console.log("Rating rendering")
-    return (
-        <div>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </div>
-    );
-}
-
-function Star() {
-    console.log("Star rendering")
-    return <div>Star</div>
-}
-
-function Accordion() {
-    console.log("Accordion rendering")
-    return (
-        <div>
-            <h3>Меню</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-        </div>
-    );
 }
 
 export default App;
