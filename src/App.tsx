bf77ccc603a2ff5f5e0986529fd289d717cf1276
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2022-11-05 22:49:57 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ function App() {
             <PageTitle title={"My friends"}/>
             Article 1
             <Rating value={3}/>
-            <Accordion title={"Menu"}/>
-            <Accordion title={"Users"}/>
+            <Accordion titleValue={"Menu"}/>
+            <Accordion titleValue={"Users"}/>
             Article 2
             <Rating value={0}/>
             <Rating value={1}/>
@@ -24,7 +24,11 @@ function App() {
     );
 }
 
-function PageTitle(props: any) {
+type PageTitlePropsTitle = {
+    title: string;
+}
+
+function PageTitle(props: PageTitlePropsTitle) {
     console.log("PageTitle rendering")
     return <h1>{props.title}</h1>
 }
