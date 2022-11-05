12e4f4b54c6751c74ec14b0e0a6f9a6ed43bc21a
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2022-11-05 20:41:53 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,6 @@ import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
 
-function hello() {
-    debugger
-    alert('Hello IT-KAMASUTRA')
-}
-
-// hello();
-
 function App() {
     console.log("App rendering")
     return (
